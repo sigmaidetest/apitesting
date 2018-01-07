38827d01c440241dca5b7e87dc1f172d4e795acd
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-07 08:34:52 +0530

    First commit

---
+++ apitesting/apitesting-lambda.js
@@ -0,0 +1,6 @@
+let AWS = require('aws-sdk');
+
+    exports.handler = function(event, context, callback){
+        console.log("Received parameters", event);
+        callback(null, "success");
+    }
